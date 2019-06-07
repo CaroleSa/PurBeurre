30eda1baac4d07727550186b758370881a2103b9
CaroleSa <carole220287@hotmail.com>
2019-06-07 16:24:41 +0200

    main.py ok but without data use

--- main.py
+++ main.py
@@ -19,7 +19,12 @@ class User:
         print("\nRenseignez le numéro de la catégorie choisie :")
         print("choix", number, ">", category)
         user_answer_category = input("Votre choix : ")
-        self.answer_choice_1_food()
+
+        if str(user_answer_category) == "1":
+            self.answer_choice_1_food()
+        else:
+            print("\nCE CHOIX N'EXISTE PAS. \nVeuillez taper un chiffre.")
+            self.answer_choice_1_category()
 
     def answer_choice_1_food(self):
         # If the user chooses the option 1,
@@ -29,7 +34,25 @@ class User:
         print("\nRenseignez le numéro de l'aliment choisi :")
         print("choix", number, ">", food)
         user_answer_food = input("Votre choix : ")
-        self.proposed_substitute_favorite()
+
+        if str(user_answer_food) == "1":
+            self.proposed_substitute_favorite()
+        else:
+            print("\nCE CHOIX N'EXISTE PAS. \nVeuillez taper un chiffre.")
+            self.answer_choice_1_food()
+
+    def save_substitute(self, substitute):
+        # Confirmation of registration
+        print("\nSouhaitez-vous enregistrer ce substitut ? \nchoix 1 > oui \nchoix 2 > non")
+        user_answer_save_food = input("Votre choix : ")
+
+        if user_answer_save_food == "1":
+            print("\nNous avons bien enregistré le substitut", substitute+".")
+        elif user_answer_save_food == "2":
+            print("\nEnregistrement non effectué pour le substitut", substitute+".")
+        else:
+            print("\nCE CHOIX N'EXISTE PAS. \nVeuillez taper 1 ou 2.")
+            self.save_substitute(substitute)
 
     def proposed_substitute_favorite(self):
         # Detail of the proposed food substitute and choice to save it : PREVOIR SI CHOIX INEXISTANT
@@ -37,20 +60,15 @@ class User:
         description = "white"
         store = "auchan"
         link = "http..."
-        print("\nSubstitut proposé :", substitute, "\nDescription :", description, "\nMagasin où le trouver :", store, "\nLien internet :", link)
-        print("\nSouhaitez-vous enregistrer ce substitut ? \nchoix 1 > oui \nchoix 2 > non")
-        user_answer_save_food = input("Votre choix : ")
-
-        # Confirmation of registration
-        if user_answer_save_food == "1":
-            print("\nNous avons bien enregistré le substitut", substitute+".")
-        elif user_answer_save_food == "2":
-            print("\nEnregistrement non effectué pour le substitut", substitute+".")
+        print("\nSubstitut proposé :", substitute, "\nDescription :", description, "\nMagasin où le trouver :",
+              store, "\nLien internet :", link)
+        self.save_substitute(substitute)
 
     def answer_choice_2(self):
         # If the user chooses the option 2 :
         print("\nMes aliments substitués enregistrés :")
-        print("\nSubstitute (substitut de food) \nDescription : descrition \nMagasin où le trouver : store \nLien internet : link")
+        print("\nSubstitute (substitut de food) \nDescription : descrition \nMagasin où le trouver : store "
+              "\nLien internet : link")
 
     def first_question(self):
         # First question at the user :
@@ -59,15 +77,12 @@ class User:
         user_answer = input("Tapez votre choix : ")
 
         if str(user_answer) == "1":
-            self.answer_choice_1()
-
+            self.answer_choice_1_category()
         elif str(user_answer) == "2":
             self.answer_choice_2()
-
         else:
-            while str(user_answer) != "1" and str(user_answer) != "2":
-                print("\nCe choix n'existe pas.")
-                self.first_question()
+            print("\nCE CHOIX N'EXISTE PAS. \nVeuillez taper 1 ou 2.")
+            self.first_question()
 
 new_user = User()
 new_user.first_question()

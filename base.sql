c1895100a741f467b011c9116443ed52f6ed5a5d
CaroleSa <carole220287@hotmail.com>
2019-06-12 10:17:34 +0200

    database.py function creation_database() works ok

--- base.sql
+++ base.sql
@@ -3,36 +3,43 @@ CREATE DATABASE Purbeurre;
 USE Purbeurre;
 
 CREATE TABLE Food(
-id INT AUTO_INCREMENT PRIMARY KEY,
+id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 name_food VARCHAR(50) NOT NULL,
 nutriscore VARCHAR(3) NOT NULL,
-description VARCHAR(500),
+description VARCHAR(1000),
 store VARCHAR(100),
 link VARCHAR(300)
 );
 
 CREATE TABLE Favorite(
-id INT AUTO_INCREMENT PRIMARY KEY,
-id_food INT,
+id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+id_food INT UNSIGNED NOT NULL,
 substitute_chooses VARCHAR(50) NOT NULL
-);
+)
+ENGINE = InnoDB;
 
 CREATE TABLE Category(
-id INT AUTO_INCREMENT NOT NULL UNIQUE,
+id INT UNSIGNED AUTO_INCREMENT NOT NULL UNIQUE,
 categories VARCHAR(50)
 );
 
 CREATE TABLE Food_category(
-id_food INT NOT NULL,
-id_category INT NOT NULL
-);
+id_food INT UNSIGNED NOT NULL,
+id_category INT UNSIGNED NOT NULL
+)
+ENGINE = InnoDB;
 
-ALTER TABLE Category ADD PRIMARY KEY (categories);
+ALTER TABLE Category
+ADD PRIMARY KEY (categories);
 
-ALTER TABLE Food ADD INDEX ind_nutriscore (nutriscore);
+ALTER TABLE Food
+ADD INDEX ind_nutriscore (nutriscore);
 
-ALTER TABLE Favorite ADD CONSTRAINT fk_favorite_id_food FOREIGN KEY (id_food) REFERENCES Food (id);
+ALTER TABLE Favorite
+ADD CONSTRAINT fk_favorite_id_food FOREIGN KEY (id_food) REFERENCES Food (id);
 
-ALTER TABLE Food_category ADD CONSTRAINT fk_food_category_id_food FOREIGN KEY (id_food) REFERENCES Food (id);
+ALTER TABLE Food_category
+ADD CONSTRAINT fk_food_category_id_food FOREIGN KEY (id_food) REFERENCES Food (id);
 
-ALTER TABLE Food_category ADD CONSTRAINT fk_food_category_id_category FOREIGN KEY (id_category) REFERENCES Category (id);
+ALTER TABLE Food_category
+ADD CONSTRAINT fk_food_category_id_category FOREIGN KEY (id_category) REFERENCES Category (id);
--- database.py
+++ database.py
@@ -15,45 +15,54 @@ class Database:
         self.data_base = mysql.connector.connect(user='root', password='Root', host='localhost')
         self.cursor = self.data_base.cursor()
 
-    def creation_database(self):
-        # running file "base.sql" requests : for the creation of the database NE MARCHE PAS
-        try:
-            self.cursor.execute("SOURCE base.sql;")
-            print('ça a marché !!!!')
-        except mysql.connector.errors.ProgrammingError:
-            print('je ne sais pas pourquoi ça ne marche pas !!!')
-        finally:
-            self.data_base.close()
+
 
     def load_insert_data(self):
         # loading data of API Openfoodfacts, convert to json and inserting data into the database
-        r = requests.get("https://fr.openfoodfacts.org/cgi/search.pl?action=process&tagtype_0=categories&tag_contains_"
+        try:
+            r = requests.get("https://fr.openfoodfacts.org/cgi/search.pl?action=process&tagtype_0=categories&tag_contains_"
                          "0=contains&tag_0=pizza&sort_by=unique_scans_n&page_size=10&axis_x=energy&axis_"
                          "y=products_n&action=display&json=1")
-        data = json.loads(r.text)
+            data = json.loads(r.text)
 
-        for value in data['products']:
-            product_name = value['product_name_fr']
-            categories = value['categories']
-            nutrition_grade = value['nutrition_grade_fr']
-            ingredients = value['ingredients_text_with_allergens']
-            store_tags = value['stores_tags']
-            url = value['url']
-            """data = {"name_food" : product_name, "category" : categories, "nutriscore" : nutrition_grade, 
-                    "description" : ingredients, "store" : store_tags, "link" : url}"""
-            print(categories)
+            for value in data['products']:
+                product_name = value['product_name_fr']
+                categories = value['categories']
+                nutrition_grade = value['nutrition_grade_fr']
+                ingredients = value['ingredients_text']
+                store_tags = value['stores_tags']
+                url = value['url']
 
-            use_data = ("USE purbeurre;")
-            insert_data = ("""INSERT INTO Food (name_food, category, nutriscore, description, store, link) 
-            VALUES({}, {}, {}, {}, {}, {});""".format(product_name, categories, nutrition_grade, ingredients,
-                                                      store_tags, url))
-            self.cursor.execute(use_data)
-            self.cursor.execute(insert_data)
-            self.data_base.commit()
-            self.cursor.close()
+                data = {"name_food": product_name, "nutriscore": nutrition_grade, "description": ingredients,
+                        "store": store_tags, "link": url}
+
+                insert_data = ("""INSERT INTO Food (name_food, nutriscore, description, store, link) 
+                            VALUES(:name_food, :nutriscore, :description, :store, :link);""", data)
+
+                self.cursor.execute("USE Purbeurre;")
+                self.cursor.execute(insert_data)
+                self.data_base.commit()
+                self.cursor.close()
+
+        except (mysql.connector.errors.OperationalError, mysql.connector.errors.DatabaseError) as m:
+            print("ca ne marche pas non plus, voici le message d'erreur :", m)
+
+        finally:
+            self.data_base.close()
+            print("MySQL est fermé")
+
+    def creation_database(self):
+        # running file "base.sql" requests : for the creation of the database
+        with open("base.sql", "r") as file:
+            base = file.read()
+            self.cursor.execute(base)
+            print('Création de la database ok !!!!')
+            self.load_insert_data()
+
+
 
 new_database = Database()
-#new_database.creation_database()
-new_database.load_insert_data()
+new_database.creation_database()
+#new_database.load_insert_data()
 
 

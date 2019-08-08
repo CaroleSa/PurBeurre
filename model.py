2ae01fad3c971bd40c562e35f45026b8812fb706
CaroleSa <carole220287@hotmail.com>
2019-08-08 13:27:28 +0200

    model.py ok

--- model.py
+++ model.py
@@ -1,26 +1,30 @@
 #! /usr/bin/env python3
 # coding: UTF-8
 
+
 """ Class Categories, Foods and Favorite """
-
-
-
-# imports
-import orm
-import controller
 
 
 class Categories:
 
-    def __init__(self):
-
-        self.new_orm = orm.Orm()
-        self.all_id_name_categories = self.new_orm.create_categories_list()
+    def __init__(self, id, categories):
+        self.id = id
+        self.categories = categories
 
 class Foods:
 
-    def __init__(self):
-        self.new_controller = controller.Controller()
-        print("j'affiche", self.new_controller.user_answer_id_category)
-        self.new_orm = orm.Orm()
-        self.all_id_name_foods = self.new_orm.create_foods_list(self.new_controller.user_answer_id_category)
+    def __init__(self, id, name_food, category_id, nutriscore, description, store, link):
+        self.id = id
+        self.name_food = name_food
+        self.category_id = category_id
+        self.nutriscore = nutriscore
+        self.description = description
+        self.store = store
+        self.link = link
+
+class Favorite:
+
+    def __init__(self, id, id_food, id_substitute_chooses):
+        self.id = id
+        self.id_food = id_food
+        self. id_substitute_chooses = id_substitute_chooses
